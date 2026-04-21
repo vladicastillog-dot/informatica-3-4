cb420e94d2a21c7eb78aea4dd243efd25ecc3084
vladicastillog-dot <vladicastillog@gmail.com>
2026-04-21 21:43:00 +0000

    hola arturo

---
+++ naming-variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int number; //I ereased !
+int test_value; // i used_instead of the space
+int n1example; // i stared the variable with a letter
+float intburger; // i didnt let int be the name of the variable so i putted
+printf("Variables created successfully.\n");
+}
