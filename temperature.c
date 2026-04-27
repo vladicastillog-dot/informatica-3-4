767358a9357d7ab4fc526fb7c0bfa51cd8fe7daa
vladicastillog-dot <vladicastillog@gmail.com>
2026-04-27 17:09:57 +0000

    kinis 66666

--- temperature.c
+++ temperature.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void){
-    int F;
-    int C;
-    printf("Farenheit = ");
-
+    int F; //Aqui el profe me dijo que le hiciera con int y inici una variable indefinida
+    printf("Farenheit = "); //hace que en la parte negra aparesca Farenheit =
+    scanf("%d" , &F);
+    float c = (F-32)/1.8; // float me da decimales
+    printf("Farenheit: %d \n" , F);
+    printf("celcious: %f \n" , c ); //la temperatura para que sea igual es menos 40
 }
