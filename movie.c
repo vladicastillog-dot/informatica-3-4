37636dd33b384853ba28383e57e1559db389bbd1
vladicastillog-dot <vladicastillog@gmail.com>
2026-04-28 21:57:28 +0000

    no pude

---
+++ movie.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+
+    int mov_run_t;
+    int start_t_h;
+    int start_t_m;
+    int end_t;
+        printf("how long is the movie? \n");
+    scanf("%d", &mov_run_t);
+    printf ("what is the starting time \n");
+    scanf("%d""%d",&start_t_h, &start_t_m");
+    
+
+
+}
